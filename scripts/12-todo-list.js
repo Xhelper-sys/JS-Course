55fdf5db59780f3f7810f03c4795257196cad6fd
Xhelper-sys <mus.kya21@gmail.com>
2026-06-18 14:08:30 +0000

    Exercice 12 bis

--- scripts/12-todo-list.js
+++ scripts/12-todo-list.js
@@ -1,6 +1,6 @@
 const todoList = [
   {name: 'make dinner', dueDate: '2022-12-22'},
-  {name: 'wash dinner', durDate: '2022-12-22'}
+  {name: 'wash dinner', dueDate: '2022-12-22'}
 ];
 
 renderTodoList();
@@ -8,7 +8,7 @@ renderTodoList();
 function renderTodoList() {
   let todoListHTML = '';
 
-  todoList.forEach(function(todoObject, index){
+  todoList.forEach((todoObject, index) => {
     const {name, dueDate} = todoObject;
     const html = `
                   <div>
@@ -17,17 +17,23 @@ function renderTodoList() {
                   <div>
                     ${dueDate} 
                   </div>
-                  <button onclick="
-                    todoList.splice(${index},1);
-                    renderTodoList();
-                  " class="delete-todo-button">
+                  <button class="delete-todo-button js-delete-todo-button">
                     Delete
                   </button>
                   `;
     todoListHTML += html;
   });
   document.querySelector('.js-todo-list').innerHTML = todoListHTML;
-}
+
+  document.querySelectorAll('.js-delete-todo-button').forEach((deleteButton, index) => {
+    deleteButton.addEventListener('click', () => {
+      todoList.splice(index,1);
+      renderTodoList();
+    });
+  });
+
+document.querySelector('.js-add-todo-button').addEventListener('click', () => {addTodo()})
+};
 
 function addTodo(){
   const inputElement = document.querySelector('.js-name-input');
